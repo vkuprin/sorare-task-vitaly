0e645130f5cf32e7f402ddad56157a2e0f3ebd94
Vitaly Kuprin <vitas_s@inbox.lv>
2022-11-10 04:46:07 +0100

    card reveal logic

--- src/components/CardBox.tsx
+++ src/components/CardBox.tsx
@@ -1,33 +1,39 @@
 import { useQuery } from '@apollo/client';
 import { GET_CARD_BY_SLUG } from '../gql/Query';
 import styled from 'styled-components';
+import CardsPlaceholder from '../assets/cards-placeholder.svg';
 
 interface CardBoxProps {
   slug: string;
+  skip?: boolean;
 }
 
-const CardBox = ({ slug }: CardBoxProps) => {
+const CardBox = ({ slug, skip }: CardBoxProps) => {
   const { data, loading } = useQuery(GET_CARD_BY_SLUG, {
-    variables: {
-      slug,
-    },
+    variables: { slug },
+    skip,
   });
 
   if (loading) return <div>Loading...</div>;
-  if (!data) return null;
 
   return (
     <Container>
-      <ImageCard src={data?.card.pictureUrl} alt={data?.card.name} />
-      <Section>
-        <ul style={{ color: 'white' }}>
-          <Title>{data?.card.name}</Title>
-          <li>Age: {data?.card.age}</li>
-          <li>Blockchain: {data?.card.blockchain}</li>
-          <li>Can Buy: {data?.card.canBuy ? 'Yes' : 'No'}</li>
-          <li>Can Sell: {data?.card.canSell ? 'Yes' : 'No'}</li>
-        </ul>
-      </Section>
+      <ImageCard
+        src={data?.card.pictureUrl || CardsPlaceholder}
+        alt={data?.card.name}
+        skip={skip}
+      />
+      {data && (
+        <Section>
+          <ul style={{ color: 'white' }}>
+            <Title>{data?.card.name}</Title>
+            <li>Age: {data?.card.age}</li>
+            <li>Blockchain: {data?.card.blockchain}</li>
+            <li>Can Buy: {data?.card.canBuy ? 'Yes' : 'No'}</li>
+            <li>Can Sell: {data?.card.canSell ? 'Yes' : 'No'}</li>
+          </ul>
+        </Section>
+      )}
     </Container>
   );
 };
@@ -42,11 +48,13 @@ const Container = styled.div`
   margin: 20px;
 `;
 
-const ImageCard = styled.img`
+const ImageCard = styled.img<{ skip?: boolean }>`
   width: 100%;
   max-width: 420px;
   display: block;
-  margin-right: 80px;
+  margin-right: ${(props) => {
+    return props.skip ? 0 : '80px';
+  }};
 `;
 
 const Title = styled.h1`
@@ -68,6 +76,16 @@ const Section = styled.section`
   border-radius: 10px;
   padding: 20px;
   box-sizing: content-box;
+
+  animation: fadein 3s;
+  @keyframes fadein {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
 `;
 
 export default CardBox;
--- src/pages/SpecificPage.tsx
+++ src/pages/SpecificPage.tsx
@@ -1,18 +1,27 @@
 import styled from 'styled-components';
 import CardBox from '../components/CardBox';
 import { useLocation } from 'react-router-dom';
+import { useState } from 'react';
 
 const SpecificPage = () => {
+  const [reveal, setReveal] = useState(true);
   const location = useLocation();
   const slugsURL = location.pathname.split('/cards/')[1].split(',');
 
   const renderCards = () =>
     slugsURL.map((slug: string, index: number) => {
       if (!slug) return null;
-      return <CardBox key={index} slug={slug} />;
+      return <CardBox key={index} slug={slug} skip={reveal} />;
     });
 
-  return <Center>{renderCards()}</Center>;
+  const handleReveal = () => setReveal(!reveal);
+
+  return (
+    <Center>
+      <RevealButton onClick={handleReveal}>Card Reveal</RevealButton>
+      {renderCards()}
+    </Center>
+  );
 };
 
 const Center = styled.div`
@@ -23,6 +32,20 @@ const Center = styled.div`
   flex-direction: column;
   flex-wrap: wrap;
   margin-top: 100px;
+`;
+
+const RevealButton = styled.button`
+  background-color: #4caf50;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+  border-radius: 10px;
 `;
 
 export default SpecificPage;
